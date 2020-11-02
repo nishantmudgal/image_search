96e2082f0708378092682dd29297ec948f36dd68
nishantmudgal <3nishantmudgal@gmail.com>
2020-11-02 13:08:32 +0530

    Added build files for firebase

---
+++ build/precache-manifest.d1bf74d029f3e8fd894abe19bbf5f65b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a1369581379f024e1d9",
+    "url": "/static/css/main.378935ed.chunk.css"
+  },
+  {
+    "revision": "6a1369581379f024e1d9",
+    "url": "/static/js/main.eaf01020.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2ca39a59a136acfc518b",
+    "url": "/static/js/2.20739868.chunk.js"
+  },
+  {
+    "revision": "c94a7ac1bdbc0e3b487fb579191ad7df",
+    "url": "/index.html"
+  }
+];
